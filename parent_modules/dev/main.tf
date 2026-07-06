c36668bc1c8104139801b948778a35b92cef867e
Bobby Soni <bobbysoni05@gmail.com>
2026-07-06 14:16:40 +0530

    tfvars added

--- parent_modules/dev/main.tf
+++ parent_modules/dev/main.tf
@@ -1,10 +1,5 @@
 module "resource_group"{
     source = "../../child_modules/01_azurerm_resource_group"
-    resource_group = {
-    rg1 = {
-        name = "bobby"
-        location = "centralindia"
-    }
-}
+   resource_group = var.resource_group
 }
 
